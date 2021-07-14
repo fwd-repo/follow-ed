cbe529a4f8fc90531850513988be227ecd4cbd4c
Ed Tansey <office.fwd@yahoo.com>
2021-07-14 19:29:54 +0100

    set up header and footer

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function followed_theme_support(){
+	// Adds dynamic title tag support
+	add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme','followed_theme_support');
+
 function followed_register_styles(){
 
 	$version = wp_get_theme()->get( 'Version' );
@@ -10,5 +17,16 @@ function followed_register_styles(){
 }
 
 add_action('wp_enqueue_scripts', 'followed_register_styles');
+
+function followed_register_scripts(){
+
+	wp_enqueue_script('followed-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1',true);
+	wp_enqueue_script('followed-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0',true);
+	wp_enqueue_script('followed-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1',true);
+	wp_enqueue_script('followed-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0',true);
+
+}
+
+add_action('wp_enqueue_scripts', 'followed_register_scripts');
 
 ?>
